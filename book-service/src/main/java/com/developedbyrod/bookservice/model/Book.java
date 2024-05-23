f87f49db32af0663c7e7fe94decfa40c2e65b0b3
codeasrof <rar2023@icloud.com>
2024-05-22 21:29:12 -0300

    Book model

---
+++ book-service/src/main/java/com/developedbyrod/bookservice/model/Book.java
@@ -0,0 +1,98 @@
+package com.developedbyrod.bookservice.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Book {
+    private Long id;
+    private String author;
+    private Date launchDate;
+    private double price;
+    private String title;
+    private String currency;
+    private String environment;
+
+
+    public Book(){
+
+    }
+
+    public Book(Long id, String author, Date launchDate, double price, String title, String currency, String environment) {
+        this.id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+        this.currency = currency;
+        this.environment = environment;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Double.compare(price, book.price) == 0 && Objects.equals(id, book.id) && Objects.equals(author, book.author) && Objects.equals(launchDate, book.launchDate) && Objects.equals(title, book.title) && Objects.equals(currency, book.currency) && Objects.equals(environment, book.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, launchDate, price, title, currency, environment);
+    }
+}
